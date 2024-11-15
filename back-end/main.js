bfa7b82d29a2c6788cda5bfc8cc8cbf1381ec913
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2024-11-15 13:56:33 +0530

    Update main.js

--- back-end/main.js
+++ back-end/main.js
@@ -21,22 +21,18 @@ const languageData = JSON.parse(fs.readFileSync(languageFilePath, "utf-8"));
 app.get("/hello", (req, res) => {
   const { language } = req.query;
 
-  console.log("Requested language:", language);  // Log the requested language
-
   if (!language) {
     return res.status(400).json({
       error_message: "The 'language' query parameter is required"
     });
   }
 
-  // Check if the language is supported
   if (!languageData[language]) {
     return res.status(400).json({
       error_message: "The requested language is not supported"
     });
   }
 
-  // Return the language message and ID
   res.status(200).json({
     message: languageData[language].msgText,
     languageID: languageData[language].ID
@@ -48,10 +44,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "front-end", "public", "main.html"));
 });
 
-// Define the port number, using either an environment variable or default to 5000
+// Set the port for the server
 const PORT = process.env.PORT || 5000;
-
-// Start the server
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running at http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
