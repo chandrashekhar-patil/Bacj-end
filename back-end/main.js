3bdd6aa54d3a2d900e596c183451648ba12699ac
Chandrashekhar Patil <156643442+chandrashekhar-patil@users.noreply.github.com>
2024-11-15 13:36:59 +0530

    Update main.js

--- back-end/main.js
+++ back-end/main.js
@@ -45,7 +45,7 @@ app.get("*", (req, res) => {
 });
 
 // Set the port for the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
